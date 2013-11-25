a81404230f2808ef9838a7e084a783fbf7b6e5fa
Kevin Waddle <kevin.waddle@gmail.com>
2013-11-25 12:51:44 -0600

    Add setuptools files.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='silverpoppy',
+    version='0.1.0',
+    description='Minimalist Silverpop Engage API library.',
+    long_description=(open('README.md').read()),
+    url='https://github.com/kevinwaddle/silverpoppy',
+    license='MIT',
+    author='Kevin Waddle',
+    author_email='kevin.waddle@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
